b3abc27b1f2d7f88257ef87db0b92d63024c67c5
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2019-10-20 23:28:48 +0000

    write triangle and fizzbuzz functions

--- enmanuel-delanuez/exercises/answers.js
+++ enmanuel-delanuez/exercises/answers.js
@@ -7,14 +7,34 @@ function triangle() {
     pascal += '#';
   }
 }
+
 // Question 2: FizzBuzz
 function fizzBuzz() {
-  // write your answer to question two below
-  
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 == 0) {
+      console.log('Fizz');
+    } else if (i % 5 == 0 && i % 3 != 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
 }
 
 // Question 3: Chess Board
 function chessBoard() {
   // write your answer to question three below
-
+  for (let i = 1; i <= 64; i++) {
+    if (i % 2 == 0) {
+      console.log('#');
+    } else {
+      console.log(' ');
+    }
+  }
+  
+  for (let i = 1; i <= 8; i++) {
+    
+  }
 }
