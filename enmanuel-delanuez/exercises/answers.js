ba61034f2edf2a81aa2874b6b83edc21c4b0ff12
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2019-10-20 17:34:15 +0000

    add code for triangle fn

--- enmanuel-delanuez/exercises/answers.js
+++ enmanuel-delanuez/exercises/answers.js
@@ -1,13 +1,16 @@
 // Question 1: Looping a Triange
 function triangle() {
   // write your answer to question one below
-
+  let pascal = '#'
+  while (pascal.length <= 7) {
+    console.log(pascal);
+    pascal += '#';
+  }
 }
-
 // Question 2: FizzBuzz
 function fizzBuzz() {
   // write your answer to question two below
-
+  
 }
 
 // Question 3: Chess Board
