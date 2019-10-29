18676f97021024129a3536b82c187f9113968c13
EC2 Default User <ec2-user@ip-172-31-16-87.us-east-2.compute.internal>
2019-10-29 04:54:54 +0000

    made changes to feedback

--- carmen-salas/exercises/answers.js
+++ carmen-salas/exercises/answers.js
@@ -17,19 +17,19 @@ function fizzBuzz() {
   
   let num = 1; 
   do{
-     if((num%3)==0){
-       if((num%5)==0){
+     if((num % 3) === 0){
+       if((num % 5) === 0){
          console.log('fizzBuzz');
        }else{
          console.log('fizz');
        }
-     }else if (num%5==0){
+     }else if (num % 5 === 0){
        console.log('buzz')
      }else{
        console.log(num);
      }
-     num+=1;
-   }while(num<100);
+     num += 1;
+   }while(num < 100);
 }
 
 // Question 3: Chess Board
@@ -38,29 +38,27 @@ function fizzBuzz() {
 //   Passing this string to console.log should show something like this:
 
 function chessBoard() {
-  for(let i=1;i<9;i++){ //loop
-    let col=1; // individual col where the hashes are plotted 
-    let row=''; //each row (empty string)
-    if (i %2 == 0){ //even rows
+  for(let i = 1;i < 9;i ++){ //loop
+    let col = 1; // individual col where the hashes are plotted 
+    let row = ''; //each row (empty string)
+    if (i % 2 === 0){ //even rows
       while(col < 9){
-        if(col %2 == 0){
+        if(col % 2 === 0){
           row += '#';
-          col += 1;
         }else{
           row += ' ';
-          col += 1;
         }
+        col += 1;
       }
       console.log(row);
     }else{ //uneven rows
       while(col < 9){
-        if(col %2 == 0){
+        if(col %2 === 0){
           row += ' ';
-          col += 1;
         }else{
           row += '#';
-          col += 1;
         }
+        col += 1;s
       }
       console.log(row);
     }
